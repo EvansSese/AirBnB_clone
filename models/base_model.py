d529e01f3bfdc0b56b569b0c3fa8f361559a09be
Evans Sese <sese_evans@yahoo.com>
2023-08-07 14:16:15 -0700

    [Feature] Added base model file

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """ Defines the base class """
     def __init__(self):
@@ -15,14 +16,15 @@ class BaseModel:
 
     def __str__(self):
         """ Returns the string representation of the model class """
-        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                self.id, self.__dict__))
 
     def save(self):
         """ Saves the data """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """ Returns a dictionary containing all keys / value of __dict__ of the instance """
+        """ Returns a dictionary containing all keys / value """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
