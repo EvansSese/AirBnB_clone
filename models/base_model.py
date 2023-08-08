33b9e16b8c46eeb10e00c9b9d13207bbc4282cf9
Evans Sese <sese_evans@yahoo.com>
2023-08-08 07:02:42 -0700

    [Feature] Task 4

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,24 @@ from datetime import datetime
 
 class BaseModel:
     """ Defines the base class """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initializes the base model """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value,"%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuida.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """ Returns the string representation of the model class """
@@ -28,4 +41,5 @@ class BaseModel:
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
         return (obj_dict)
