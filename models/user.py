36608e9ed529d4125fb86206845c939ad05b1dce
biitopher <biitopher@gmail.com>
2023-08-12 16:07:41 -0700

    [Fix]User

--- models/user.py
+++ models/user.py
@@ -7,9 +7,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """Represents user class"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
