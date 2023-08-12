e49fc7192e4d58ee7a267617f2c8dad739dfedce
biitopher <biitopher@gmail.com>
2023-08-12 15:12:29 -0700

    [Fix]Pycodestyle

--- console.py
+++ console.py
@@ -103,7 +103,6 @@ class HBNBCommand(cmd.Cmd):
 
         print([str(obj) for key, obj in all_objects.items() if args[0] in key])
 
-
     def do_update(self, arg):
         """update instance based on class name and id """
         if not arg:
